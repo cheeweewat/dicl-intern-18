1c4599aaaa39161f92410ab41cd2f240871f0c58
Cheeweewat Saisin <38095310+cheeweewat@users.noreply.github.com>
2018-04-05 17:19:03 +0700

    Update scores.java

--- scores.java
+++ scores.java
@@ -4,40 +4,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class JSON_Reader
+public class scores
 {
     public static void main(String args[])
     {
         JSONParser parser = new JSONParser();
         try
         {
-            Object object = parser.parse(new FileReader("C:\Users\Admin\dicl-intern-18\data.json"));
-        
-            JSONObject jsonObject = (JSONObject)object;
-            
-            //Reading the String
-            String name = (String) jsonObject.get("name");
-            int score = (int) jsonObject.get("score");
-            int grade = (int) jsonObject.get("grade");
-            int total=0;            
-            
-            
-            System.out.println("1.1 Average Score");
-            for (int i = 0; i < score.length; i++) {
-                total+=score;
-                System.out.println("Average score : " +total);
-            }
-            System.out.println("1.2 Who has 70+ Score");
-            for (int i = 0; i < score.length; i++) {
-                if (score>70) {
-                    System.out.println("Name : " +name);
-                    System.out.println("Score : " +score);
-                }
-            }
-
-            
-            
-            
+            JSONArray arr = (JSONArray) parser.parse(new FileReader("C:\\Users\\Admin\\dicl-intern-18\\data.json"));
+   
         }
         catch(FileNotFoundException fe)
         {
